52be97bb623b9c7f64c87b02f5b4f94a7f430bce
heymendezz <64676693+heymendezz@users.noreply.github.com>
2022-12-21 22:13:59 -0300

    validação numero correto

--- app/validacao.js
+++ app/validacao.js
@@ -1,12 +1,23 @@
 function verificaSeOChutePossuiUmValorValido(chute){
     const numero = +chute
 
-    if(Number.isNaN(numero)){
-        console.log(`Valor Invalido - Apenas numeros entre entre ${menorValor} e ${maiorValor}`)
+    if(chuteForInvalido(numero)) {
+
+        elementoChute.innerHTML += '<div>Valor inválido</div>'  
+        
     }
  
     if (numeroForMaiorOuMenorQueOValorPermitido(numero)){
-        console.log(`Valor invalido - O Numero precisa estar entre ${menorValor} e ${maiorValor}`)
+        elementoChute.innerHTML += `
+        <div>Valor invalido - O Numero precisa estar entre ${menorValor} e ${maiorValor}</div>
+        `
+    }
+
+    if (numero === numeroSecreto){
+        document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3> O número secreto era ${numero}</h3>
+        `
     }
 
 
