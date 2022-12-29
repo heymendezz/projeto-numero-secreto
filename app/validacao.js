1e9ed5423c0d8e83e55257afd5d7137d33cccec6
heymendezz <64676693+heymendezz@users.noreply.github.com>
2022-12-29 19:02:33 -0300

    Adição dicas e progressão do jogo

--- app/validacao.js
+++ app/validacao.js
@@ -4,20 +4,29 @@ function verificaSeOChutePossuiUmValorValido(chute){
     if(chuteForInvalido(numero)) {
 
         elementoChute.innerHTML += '<div>Valor inválido</div>'  
+
+        return
         
     }
  
     if (numeroForMaiorOuMenorQueOValorPermitido(numero)){
         elementoChute.innerHTML += `
-        <div>Valor invalido - O Numero precisa estar entre ${menorValor} e ${maiorValor}</div>
-        `
+        <div>Valor invalido - O Numero precisa estar entre ${menorValor} e ${maiorValor}</div>`
+        return
     }
 
     if (numero === numeroSecreto){
         document.body.innerHTML = `
         <h2>Você acertou!</h2>
-        <h3> O número secreto era ${numero}</h3>
-        `
+        <h3> O número secreto era ${numero}</h3>`  
+    }
+
+    else if (numero < numeroSecreto) {
+        elementoChute.innerHTML += `<div>O numero secreto é maior <i class="fa-solid fa-arrow-up"></i></div>`
+    }
+
+    else {
+        elementoChute.innerHTML += `<div>O numero secreto é menor <i class="fa-solid fa-arrow-down"></i></div>`
     }
 
 
